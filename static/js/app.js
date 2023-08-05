e51b08ac5f98d92d3ea87c12641dc6c8fbdc8f38
听说 <2107228359@qq.com>
2023-08-05 20:11:54 +0800

    menu菜单完成

--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,43 @@
 jQuery(document).ready(function () {
     function menu() {
+
+        // 宽度计算
         let width = 0
         let result = $('.mmui-user-card>.menu>.menu-list .item').each(function (index, item) {
             width += $(item).text().trim().length * 20
         })
         $('.mmui-user-card>.menu>.menu-list').width(width)
 
-        $('#menu-active').click(function () {
-            $('.mmui-user-card .menu').animate({
-                opacity:'toggle'
-            })
+        let timer = null
+        // 进入active 或者退出 active
+        $('#menu-active').hover(function () {
+            $('.mmui-user-card .menu').show()
+        }, function () {
+            timer = setTimeout(function () {
+                $('.mmui-user-card .menu').hide()
+            }, 1500)
+        })
+
+
+        $('.mmui-user-card .menu').on('mouseenter', function () {
+            // $('.mmui-user-card .menu').show()
+            console.log(123)
+            clearTimeout(timer)
+        })
+
+        $('.mmui-user-card .menu').on('mouseleave', function () {
+            $('.mmui-user-card .menu').hide()
         })
     }
     menu()
 
     $(document)
-        .on('pjax:send',function() {
+        .on('pjax:send', function () {
             //加载动画效果开始
             NProgress.start();
             ajaxcomments()
 
-        }).on('pjax:complete',function() {
+        }).on('pjax:complete', function () {
             //加载动画效果结束
             NProgress.done();
             ajaxcomments()
@@ -29,5 +46,5 @@ jQuery(document).ready(function () {
             $('.mmui-user-card .menu').fadeOut()
             ajaxcomments()
 
-    })
+        })
 })
