0a274327f38289ef809347e3e06dc316aff04ddf
听说 <2107228359@qq.com>
2023-08-11 11:32:52 +0800

    pjax 修改

--- static/js/app.js
+++ static/js/app.js
@@ -116,6 +116,7 @@ jQuery(document).ready(function () {
     }).on('pjax:end', function () {
       // pjax结束时运行代码
       $('.mmui-user-card .menu').fadeOut()
+      $('.header-comment-content').fadeIn()
       // ajaxcomments()
     })
 })
