e9ce6056af7654d686912ca04ef39158a20f2bca
codemmk <2107228359@qq.com>
2023-11-08 06:48:30 +0800

    commit

---
+++ Yuna/index.php
@@ -0,0 +1,8 @@
+Html Webpack Plugin:
+<pre>
+  Error: HtmlWebpackPlugin: could not load file /Users/od23/Desktop/BLOG/usr/themes/listen-dev/listen-vue/public/favicon.ico
+  
+  - index.js:543 
+    [listen-vue]/[html-webpack-plugin]/index.js:543:35
+  
+</pre>
--- listen-vue/src/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <router-view/>
-</template>
-
-<style lang="less">
-</style>
