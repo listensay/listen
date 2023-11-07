9c2d2004e204636b759fe60f7297a362cd082b24
codemmk <2107228359@qq.com>
2023-11-08 07:07:28 +0800

    commit

--- README.md
+++
@@ -1,9 +0,0 @@
-# listen
-Typecho 仿朋友圈主题
-
-演示：http://blog.200205.net (Base版本)
-
-喜欢的话，请给个小星星表示支持，（づ￣3￣）づ╭❤～
-
-- listen-base 普通版本 友好的SEO
-- listen-vue版本 功能强大 （开发中...）
--- listen-vue/src/server/index.js
+++ listen-vue/src/server/index.js
@@ -5,5 +5,5 @@ import Process from "./process";
  * 获取全部页面
  */
 export const getPages = () => {
-  return Request("/api/pages").then(res => Process.getPages(res.data.dataSet));
+  return Request("/api/pages").then(res => console.log(res));
 };
