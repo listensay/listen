8d1d08bec31755e0464c49c33fa3f3ba077aa124
codemmk <2107228359@qq.com>
2023-11-08 07:11:36 +0800

    commit

--- listen-vue/src/server/index.js
+++
@@ -1,9 +0,0 @@
-import Request from "../util/request";
-import Process from "./process";
-
-/**
- * 获取全部页面
- */
-export const getPages = () => {
-  return Request("/api/pages").then(res => console.log(res));
-};
--- listen-vue/src/server/process.js
+++
@@ -1,8 +0,0 @@
-// 接口数据清洗
-export default {
-  getPages: async d => {
-    let pagesList = [];
-    for (let item of d) if (item) pagesList.push(item);
-    return pagesList;
-  }
-};
