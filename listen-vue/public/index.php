c1bc35bb8f9bbb05114d5f8cecc0be545b2c37c9
codemmk <2107228359@qq.com>
2023-11-07 20:41:33 +0800

    commit

---
+++ listen-vue/public/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 朋友圈主题啦！
+ *
+ * @package Typecho 朋友圈主题
+ * @author 星期四
+ * @version 2.1.0
+ * @link http://blog.200205.net
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+?>
+<!doctype html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="icon" href="favicon.ico">
+  <title>listen-vue</title>
+  <script defer="defer" src="js/chunk-vendors.860be46c.js"></script>
+  <script defer="defer" src="js/app.22c74b9c.js"></script>
+  <link href="css/app.32532972.css" rel="stylesheet">
+</head>
+
+<body><noscript><strong>We're sorry but listen-vue doesn't work properly without JavaScript enabled. Please enable it to
+      continue.</strong></noscript>
+  <div id="app"></div>
+</body>
+
+</html>
