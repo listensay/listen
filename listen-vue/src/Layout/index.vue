9b67a1323235be5d870d132bee162c6adf84bd0a
codemmk <2107228359@qq.com>
2023-11-07 21:19:37 +0800

    commit

---
+++ listen-vue/src/Layout/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="layout">
+    <div>123</div>
+      <router-view/>
+    <div>123</div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
--- listen-vue/vue.config.js
+++ listen-vue/vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const { VantResolver } = require('@vant/auto-import-resolver')
+const ComponentsPlugin = require('unplugin-vue-components/webpack')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  publicPath: './',
+  outputDir: 'listen',
+  configureWebpack: {
+    plugins: [
+      ComponentsPlugin({
+        resolvers: [VantResolver()]
+      })
+    ]
+  }
 })
