d8554bedd646dd99b9dc91fbc9cae65a0ebefb4f
Miliya <odr233@gmail.com>
2024-04-19 11:28:02 +0800

    0.0.5

--- listen-vue/index.php
+++ listen-vue/index.php
@@ -4,7 +4,7 @@
  *
  * @package listen-vue
  * @author 星期四
- * @version 0.0.4
+ * @version 0.0.5
  * @link http://blog.200205.net
  */
 
@@ -14,4 +14,4 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
           'search' => _t('包含关键字 %s 的文章'),
           'tag' => _t('标签 %s 下的文章'),
           'author' => _t('%s 发布的文章')
-      ], '', ' - '); ?> <?php $this->options->title(); ?> </title><script defer="defer" src="/usr/themes/listen-vue/js/chunk-vendors.b5bebe03.js"></script><script defer="defer" src="/usr/themes/listen-vue/js/index.3a5cec78.js"></script><link href="/usr/themes/listen-vue/css/chunk-vendors.67a2fae0.css" rel="stylesheet"><link href="/usr/themes/listen-vue/css/index.faff34e2.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but listen-vue-dev doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
+      ], '', ' - '); ?> <?php $this->options->title(); ?> </title><script defer="defer" src="/usr/themes/listen-vue/js/chunk-vendors.7fcc53bf.js"></script><script defer="defer" src="/usr/themes/listen-vue/js/index.6e06cf99.js"></script><link href="/usr/themes/listen-vue/css/chunk-vendors.67a2fae0.css" rel="stylesheet"><link href="/usr/themes/listen-vue/css/index.068b730d.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but listen-vue-dev doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
